7bf17def4600b6d352b396dcd4ec39f44f72c7fc
CriticalFlaw <nik_101@live.co.uk>
2019-08-23 19:20:47 -0400

    Project Base
    - Added the base project files
    - Added the WolfCurses console library via nuget
    - Added basic console and core game handlers

---
+++ src/TeufortTrail/GameCore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using WolfCurses;
+
+namespace TeufortTrail
+{
+    public class GameCore : SimulationApp
+    {
+        public static GameCore Instance { get; private set; }
+        public int TotalTurns { get; private set; }
+
+        public override IEnumerable<Type> AllowedWindows
+        {
+            get
+            {
+                var windowList = new List<Type>
+                {
+                    // TODO: Add screen types
+                };
+
+                return windowList;
+            }
+        }
+
+        public static void Create()
+        {
+            if (Instance != null) throw new InvalidOperationException("A game instance already exists!");
+            Instance = new GameCore();
+        }
+
+        public override string OnPreRender()
+        {
+            return $"Turns: {TotalTurns:D4}\nLocation: UNKNOWN";
+        }
+
+        protected override void OnFirstTick()
+        {
+            TotalTurns = 0;
+
+            // TODO: Initialize screens
+
+            // Reset the Window Manager
+            base.Restart();
+        }
+
+        protected override void OnPreDestroy()
+        {
+            TotalTurns = 0;
+            Instance = null;
+        }
+
+        public void TakeTurn()
+        {
+            TotalTurns++;
+        }
+    }
+}
---
+++ src/TeufortTrail/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace TeufortTrail
+{
+    internal static class Program
+    {
+        public static int Main()
+        {
+            // Setup the console
+            Console.Title = "Teufort Trail";
+            Console.WriteLine("Loading...");
+            Console.CursorVisible = false;
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            // Setup the game instance
+            GameCore.Create();
+            GameCore.Instance.SceneGraph.ScreenBufferDirtyEvent += ScreenBufferDirtyEvent;
+
+            while (GameCore.Instance != null)
+            {
+                GameCore.Instance.OnTick(true);
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Enter:
+                            GameCore.Instance.InputManager.SendInputBufferAsCommand();
+                            break;
+
+                        case ConsoleKey.Backspace:
+                            GameCore.Instance.InputManager.RemoveLastCharOfInputBuffer();
+                            break;
+
+                        default:
+                            GameCore.Instance.InputManager.AddCharToInputBuffer(key.KeyChar);
+                            break;
+                    }
+                }
+                Thread.Sleep(1);
+            }
+
+            // Clean the console
+            Console.Clear();
+            Console.WriteLine("Goodbye!");
+            Console.WriteLine("Press ANY KEY to close this window...");
+            Console.ReadKey();
+            return 0;
+        }
+
+        private static void ScreenBufferDirtyEvent(string input)
+        {
+            var content = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            for (var index = 0; index < Console.WindowHeight - 1; index++)
+            {
+                Console.CursorLeft = 0;
+                Console.SetCursorPosition(0, index);
+                var output = new string(' ', Console.WindowWidth);
+                if (content.Length > index)
+                    output = content[index].PadRight(Console.WindowWidth);
+                Console.Write(output);
+            }
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
