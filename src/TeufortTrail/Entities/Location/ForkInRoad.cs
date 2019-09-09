3754e7b66fe8a11c41cec6fe917a3abac86df0f0
CriticalFlaw <nik_101@live.co.uk>
2019-09-09 16:58:34 -0400

    Fork Roads
    - Added a fork road location variant, allowing the player to choose the direction they want to travel in.

---
+++ src/TeufortTrail/Entities/Location/ForkInRoad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TeufortTrail.Entities.Location
+{
+    public sealed class ForkInRoad : Location
+    {
+        #region VARIABLES
+
+        /// <summary>
+        /// Flags the location as having a store that the player can visit.
+        /// </summary>
+        public override bool ShoppingAllowed => false;
+
+        /// <summary>
+        /// Flags the location as having people that the player can talk to.
+        /// </summary>
+        public override bool TalkingAllowed => false;
+
+        /// <summary>
+        /// List of possible paths that the player can choose from.
+        /// </summary>
+        public ReadOnlyCollection<Location> PathChoices => _pathChoices.AsReadOnly();
+
+        public readonly List<Location> _pathChoices;
+
+        #endregion VARIABLES
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:TeufortTrail.Entities.Location.ForkInRoad" /> class.
+        /// </summary>
+        /// <param name="name">Name of the location</param>
+        public ForkInRoad(string name, IEnumerable<Location> paths) : base(name)
+        {
+            if (paths != null) _pathChoices = new List<Location>(paths);
+        }
+    }
+}
---
+++ src/TeufortTrail/Screens/Toll/ForkRoad.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TeufortTrail.Entities.Location;
+using TeufortTrail.Screens.Travel.Commands;
+using WolfCurses.Window;
+using WolfCurses.Window.Form;
+
+namespace TeufortTrail.Screens.Travel.Toll
+{
+    [ParentWindow(typeof(Travel))]
+    public sealed class ForkRoad : Form<TravelInfo>
+    {
+        #region VARIABLES
+
+        private StringBuilder _forkRoad;
+
+        private Dictionary<int, Location> PathChoices;
+
+        #endregion VARIABLES
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:TeufortTrail.Screens.Travel.Toll.ForkRoad" /> class.
+        /// </summary>
+        public ForkRoad(IWindow window) : base(window)
+        {
+        }
+
+        /// <summary>
+        /// Called when this screen has been created and now needs information to be displayed.
+        /// </summary>
+        public override void OnFormPostCreate()
+        {
+            // Create a dictionary that represents all the choices with index starting at one not zero.
+            base.OnFormPostCreate();
+            PathChoices = new Dictionary<int, Location>();
+            var forkInRoad = GameCore.Instance.Trail.CurrentLocation as ForkInRoad;
+            for (var index = 0; index < forkInRoad.PathChoices.Count; index++)
+                PathChoices.Add(index + 1, forkInRoad.PathChoices[index]);
+        }
+
+        /// <summary>
+        /// Called when the user has inputted something that needs to be processed.
+        /// </summary>
+        public override void OnInputBufferReturned(string input)
+        {
+            // Check that the user input is a valid intenger, above zero.
+            if (!int.TryParse(input, out var userInput) || userInput <= 0) return;
+
+            // Dictionary of path choices must contain key with input number.
+            if (PathChoices.ContainsKey(userInput))
+            {
+                // Generate a toll point if that's the selected location choice.
+                if (PathChoices[userInput] is TollInRoad tollRoad)
+                {
+                    UserData.GenerateToll(tollRoad);
+                    SetForm(typeof(TollRoad));
+                }
+                else
+                {
+                    GameCore.Instance.Trail.InsertLocation(PathChoices[userInput]);
+                    SetForm(typeof(ContinueTrail));
+                }
+            }
+            else
+                SetForm(typeof(CheckMap));
+        }
+
+        /// <summary>
+        /// Returns the text-only representation of the current game screen.
+        /// </summary>
+        public override string OnRenderForm()
+        {
+            // Clear the string builder and being building a new fork in the road based on current location skip choices.
+            _forkRoad = new StringBuilder();
+            _forkRoad.AppendLine($"{Environment.NewLine}You encounter a fork in the road. You may:{Environment.NewLine}");
+            foreach (var pathChoice in PathChoices)
+            {
+                _forkRoad.AppendLine($"{pathChoice.Key}. Travel to {pathChoice.Value.Name}");
+                if (pathChoice.Key == PathChoices.Last().Key)
+                    _forkRoad.Append($"{pathChoice.Key + 1}. Check the Map");
+            }
+            return _forkRoad.ToString();
+        }
+    }
+}
