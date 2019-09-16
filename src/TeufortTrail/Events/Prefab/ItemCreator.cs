50ea6b816bed8b183d1e6ad5c5e3b58365f82e26
CriticalFlaw <nik_101@live.co.uk>
2019-09-16 17:13:51 -0400

    Event Prefabs

---
+++ src/TeufortTrail/Events/Prefab/ItemCreator.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using TeufortTrail.Entities;
+using TeufortTrail.Events.Director;
+
+namespace TeufortTrail.Events
+{
+    /// <summary>
+    /// Event prefab used when a random amount of items needs to added to the player inventory.
+    /// </summary>
+    public abstract class ItemCreator : EventProduct
+    {
+        private StringBuilder _itemCreator;
+
+        //-------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Called when the event has been created by the event director, and before it is executed.
+        /// </summary>
+        public override void OnEventCreate()
+        {
+            base.OnEventCreate();
+            _itemCreator = new StringBuilder();
+        }
+
+        /// <summary>
+        /// Called when the event directory triggers the event action.
+        /// </summary>
+        public override void Execute(EventInfo eventExecutor)
+        {
+            _itemCreator.Clear();
+
+            // Append the pre-create message if it exists.
+            var preCreateText = OnPreCreateItems();
+            if (!string.IsNullOrEmpty(preCreateText))
+                _itemCreator.AppendLine(preCreateText);
+
+            // Get a list of schema items of random type and quantity.
+            var createdItems = GameCore.Instance.Vehicle.CreateRandomItems();
+
+            // Append the post-create message if it exists.
+            var postCreateText = OnPostCreateItems(createdItems);
+            if (!string.IsNullOrEmpty(postCreateText))
+                _itemCreator.AppendLine(postCreateText);
+
+            // Skip this step if no items were created.
+            if (!(createdItems?.Count > 0)) return;
+
+            // Loop through the generated items and add them to output string.
+            foreach (var createdItem in createdItems)
+                _itemCreator.AppendLine($"{createdItem.Value:N0} {createdItem.Key}");
+        }
+
+        /// <summary>
+        /// Called when the event director needs to render the event text to the user interface.
+        /// </summary>
+        protected override string OnRender(EventInfo userData)
+        {
+            return _itemCreator.ToString();
+        }
+
+        /// <summary>
+        /// Called before the event has been executed.
+        /// </summary>
+        protected abstract string OnPreCreateItems();
+
+        /// <summary>
+        /// Called after the event has been executed.
+        /// </summary>
+        protected abstract string OnPostCreateItems(IDictionary<ItemTypes, int> createdItems);
+    }
+}
---
+++ src/TeufortTrail/Events/Prefab/PersonInfect.cs
@@ -0,0 +1,35 @@
+﻿using TeufortTrail.Events.Director;
+
+namespace TeufortTrail.Events
+{
+    /// <summary>
+    /// Event prefab used when a person in the party needs to infected.
+    /// </summary>
+    public abstract class PersonInfect : EventProduct
+    {
+        /// <summary>
+        /// Called when the event directory triggers the event action.
+        /// </summary>
+        public override void Execute(EventInfo eventExecutor)
+        {
+            // Set the infect flag on the source entity.
+            var person = eventExecutor.SourceEntity as Entities.Person.Person;
+            person?.Infect();
+        }
+
+        /// <summary>
+        /// Called when the event director needs to render the event text to the user interface.
+        /// </summary>
+        protected override string OnRender(EventInfo userData)
+        {
+            // Skip this step if the source entity is not of correct type.
+            var person = userData.SourceEntity as Entities.Person.Person;
+            return (person == null) ? string.Empty : OnPostInfection(person);
+        }
+
+        /// <summary>
+        /// Called after the event has been executed, updates target entity.
+        /// </summary>
+        protected abstract string OnPostInfection(Entities.Person.Person person);
+    }
+}
